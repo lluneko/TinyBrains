ab0df00d1a510f7fbb13f48d6b90eead5074ae6a
lluneko <117767742+lluneko@users.noreply.github.com>
2023-12-24 18:17:44 +0300

    add my game

--- data/parsing.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-db_file = '../db/users_base.sqlite'
-con = sqlite3.connect(db_file)
-cur = con.cursor()
-res = cur.execute(f"SELECT history FROM users WHERE name = 'ol'").fetchall()
-print(res[0][0])
-
